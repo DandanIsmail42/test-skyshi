b21268d94c1db17da2023edfe49a638b4718a44d
DandanIsmail42 <dandanismail42@gmail.com>
2023-03-09 22:09:41 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+import Navbar from './component/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './css/styles.css';
+import Home from './component/Home';
+import ItemList from './component/ItemList';
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/todo-list/" element={<Home />} />
+        <Route path="/detail/:id" element={<ItemList />} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
